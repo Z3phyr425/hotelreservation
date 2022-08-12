8e709c8d6432f9f3759c533507aae988baac7f23
Mark Tungol <90316200+Z3phyr425@users.noreply.github.com>
2022-08-12 13:40:07 +0800

    Update createedit.php

--- createedit.php
+++ createedit.php
@@ -26,7 +26,38 @@
         include("header.php");
     ?>
     <div id="body">
-    
+        <form action="" method="post">
+            <h2>CREATE ACCOUNT</h2>
+            </label><input type="text" class="text1" name="searchtxt" placeholder="Search..." required><input type="submit" class="btn1" name="searchbtn" value="Search">
+            <h3>Account Details</h3>
+            <hr>
+            <label for="" class="labels">Username: </label><input type="text" class="text1" name="username" required>
+            <br>
+            <br>
+            <label for="" class="labels">Password: </label><input type="password" class="text1" name="password" required>
+            <br>
+            <br>
+            <label for="" class="labels">Confirm Password: </label><input type="password" class="text1" name="cpassword" required>
+            <br>
+            <br>
+            <h3>User Details</h3>
+            <hr>
+            <label for="" class="labels">First Name: </label><input type="text" class="text1" name="firstname" required>
+            <br>
+            <br>
+            <label for="" class="labels">Surname: </label><input type="text" class="text1" name="surname" required>
+            <br>
+            <br>
+            <label for="" class="labels">Email Address: </label><input type="text" class="text1" name="email" required>
+            <br>
+            <br>
+            <label for="" class="labels">Contact Number: </label><input type="text" class="text1" name="contactnumber" required>
+            <br>
+            <br>
+            <input type="submit" class="btn1" name="createaccount" value="Create Account">
+            <input type="submit" class="btn1" name="update" value="Update">
+        </form>
+
     </div>
     <div id="footer">
 
