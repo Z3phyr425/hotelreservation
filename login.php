123429989a9d4c3823b89f8800144844cbc12e63
Mark Tungol <90316200+Z3phyr425@users.noreply.github.com>
2022-08-13 12:21:43 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
     include("connection.php");
     session_start();
+
+    if(isset($_SESSION['User'])){
+        header("location: indexportal.php");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
