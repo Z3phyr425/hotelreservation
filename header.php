34c05c2e951a8e1f3204f7efde2f8be99c15c491
norlynatonio <72895519+norlynatonio@users.noreply.github.com>
2022-08-15 01:07:15 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,42 +1,13 @@
-<div id="header">
-        <h3>
-            <?php echo $_SESSION['User'];?>
-        </h3>
-        <form action="" method="post">
-            <input type="submit" name="logout" value="Logout" class="btn1">
-            <?php
-                if(isset($_POST['logout'])){
-                    session_destroy();
-                    header("location: index.php");
-                }
-            ?>
-        </form>
-        <?php
-            if($role == "Admin"){
-                echo "  <ul>
-                            <li><a href=\"indexportal.php\">Home</a></li>
-                            <li><a href=\"\">Accounts</a>
-                                <ul>
-                                    <li><a href=\"createedit.php\">Create & Edit User</a></li>
-                                    <li><a href=\"#\">Tab2.2</a></li>
-                                </ul>
-                            </li>
-                            <li><a href=\"\">Tab3</a>
-                                <ul>
-                                    <li><a href=\"#\">Tab3.1</a></li>
-                                </ul>
-                            </li>
-                        </ul>";
-            }else{
-                echo "  <ul>
-                            <li><a href=\"indexportal.php\">Home</a></li>
-                            <li><a href=\"\">My Profile</a>
-                                <ul>
-                                    <li><a href=\"viewprofile.php\">View Profile</a></li>
-                                    <li><a href=\"editprofile.php\">Edit Profile</a></li>
-                                </ul>
-                            </li>
-                        </ul>";
-            }
-        ?>
-    </div>
+<style type="text/css">
+	.container_header{
+        height: calc(18%);
+        background: #132332;
+        margin: 0;
+        position: absolute;
+	}
+</style>
+
+<div class="container container_header m-0 mw-100 border">
+	asdsa
+</div>
+
